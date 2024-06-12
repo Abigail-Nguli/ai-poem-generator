767cf76d266c0e897cc0120d65bd7a6cb4635ca8
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2024-06-12 16:13:17 +0300

    Added an ai to generate the poem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-function addPoem(event){
-    event.preventDefault();
+function displayPoem(response){
+    
 
     new Typewriter("#poem", {
-      strings: `I love you<br>
-        I love you when you're sad<br>
-        I love you when you're happy<br>
-        I love you...Always`,
+      strings: response.data.answer,
       autoStart: true,
       delay: 15,
       cursor: "",
     });
 }
 
+function getPoem(event){
+    event.preventDefault();
+
+    let apiKey = "fa483db98dc0o4b7fc5fdbea841a31ta";
+    let searchInput = document.querySelector('#search-input');
+    let prompt = `Generate a poem about: ${searchInput.value}`;
+    let context = `The poem should not have a title and should be displayed in HTML form with each line separated by <br/> and the poem should not be more than 5 lines. The last line should be signed with <strong>SheCodes Ai</strong>`;
+    
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+}
+
 let inputSearch = document.querySelector("#submit-btn");
-inputSearch.addEventListener("click", addPoem)
+inputSearch.addEventListener("click", getPoem);
